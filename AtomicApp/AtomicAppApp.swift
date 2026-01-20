f30b4ca4767edb29e6ea1615ec42262745c2a7fa
Chi <richardmlfns@gmail.com>
2026-01-19 21:42:31 -0500

    Initial Commit

---
+++ AtomicApp/AtomicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtomicAppApp.swift
+//  AtomicApp
+//
+//  Created by 张驰 on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct AtomicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AtomicAppTests/AtomicAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AtomicAppTests.swift
+//  AtomicAppTests
+//
+//  Created by 张驰 on 1/19/26.
+//
+
+import Testing
+@testable import AtomicApp
+
+struct AtomicAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
