596e0cc4bdf5d03978ee83066451469492f5919d
Chi <richardmlfns@gmail.com>
2026-01-20 21:37:44 -0500

    Day 2: screen time app with monitoring setup

--- AtomicApp/ContentView.swift
+++ AtomicApp/ContentView.swift
@@ -6,16 +6,99 @@
 //
 
 import SwiftUI
+import FamilyControls
+import ManagedSettings
+import DeviceActivity
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    // object with shared property shared across the app
+    // checks if this app has the permission to adjust screen time
+    let center = AuthorizationCenter.shared
+    
+    // Stores which apps the user picks
+    // FamilyActivitySelection is Apple's privacy-protected struct that contains apps, websites, and categories
+    @State private var selection = FamilyActivitySelection()
+    // Controls whether the picker sheet is showing
+    @State private var isPresented = false
+    @State private var dailyLimitMinutes: Int = 5
+    // Unique identifier for your monitoring schedule
+    @State private var activityName = DeviceActivityName("DailyLimit")
+    
+    func requestAuthorization() async {
+        do {
+            try await center.requestAuthorization(for: .individual)
+        } catch {
+            print("Authorization failed: \(error)")
         }
+    }
+    
+    func setUpMonitoring() {
+        let schedule = DeviceActivitySchedule(
+            intervalStart: DateComponents(hour: 0, minute: 0),
+            intervalEnd: DateComponents(hour: 23, minute: 59),
+            repeats: true
+        )
+        
+        // Transform each item in the set and extract just its token
+        let applicationTokens = selection.applications.compactMap { $0.token }
+        
+        // Creates a unique identifier for this specific event.
+        let eventName = DeviceActivityEvent.Name("LimitReached")
+        // Struct as rule for when to trigger the event
+        let event = DeviceActivityEvent(
+            // Watch these specific apps
+            applications: Set(applicationTokens),
+            // threshold to fire
+            threshold: DateComponents(minute: dailyLimitMinutes)
+        )
+        
+        let center = DeviceActivityCenter()
+        
+        do {
+            // Pass the event to startMonitoring
+            try center.startMonitoring(
+                activityName,
+                during: schedule,
+                events: [eventName: event]
+            )
+            print("Monitoring started with \(dailyLimitMinutes) min limit")
+        } catch {
+            print("Failed to start monitoring: \(error)")
+        }
+    }
+    
+    
+    var body: some View {
+        VStack (spacing: 10){
+            Button("Select Apps to Limit") {
+                isPresented = true
+            }
+            
+            VStack (spacing: 10){
+                Text("Selected \(selection.applications.count) apps")
+                Text("Selected \(selection.categories.count) categories")
+                Text("Selected \(selection.webDomains.count) web domains")
+            }
+            .padding()
+            
+            Stepper("Daily Limit: \(dailyLimitMinutes) minutes",
+                    value: $dailyLimitMinutes,
+                    in: 5...480,
+                    step: 1)
+            .padding()
+            
+            Button("Start Monitoring") {
+                setUpMonitoring()
+            }.buttonStyle(.borderedProminent)
+        }
+        
+        // Modifiers are instructors and not sequential pieces of code
         .padding()
+        // When isPresentedis true, show the FamilyActivityPicker
+        .familyActivityPicker(isPresented: $isPresented, selection: $selection)
+        .task {
+            await requestAuthorization()
+        }
     }
 }
 
